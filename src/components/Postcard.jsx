d7be616f3d503c77d9bf5d17183ed87e74291a37
Komola Khidirova <komolakhidirova@gmail.com>
2025-12-11 19:18:57 +0500

    postcard ui

--- src/components/Postcard.jsx
+++ src/components/Postcard.jsx
@@ -36,12 +36,12 @@ function Postcard({ backgroundImage, text, from, to, onDownloadComplete }) {
 		<div className='flex flex-col items-center justify-center p-4 sm:p-6 lg:p-8'>
 			<div
 				ref={postcardRef}
-				className='relative w-full max-w-4xl h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] postcard-outer-frame'
+				className='relative w-full max-w-4xl h-[300px] max-sm:h-[250px] sm:h-[400px] md:h-[500px] lg:h-[600px] postcard-outer-frame'
 			>
 				<div className='absolute inset-2.5 max-sm:inset-1.5 overflow-hidden rounded-[10px]'>
 					<div className='relative h-full w-full'>
 						<div className='absolute top-0 right-0 z-10'>
-							<img src='/stamp.png' className='w-40 md:w-60 max-sm:w-20' />
+							<img src='/stamp.png' className='w-40 md:w-60 max-sm:w-25' />
 						</div>
 						<div
 							className='absolute inset-0 bg-cover bg-center bg-no-repeat'
@@ -51,7 +51,7 @@ function Postcard({ backgroundImage, text, from, to, onDownloadComplete }) {
 						/>
 
 						<div className='h-full w-full flex flex-col justify-start items-start relative z-10 '>
-							<div className='max-w-sm m-10 max-sm:m-2  max-sm:max-w-60 bg-white p-5 md:p-10 max-sm:p-3 rounded-sm border-2 border-dashed border-blue-950'>
+							<div className='max-w-sm m-10 max-sm:m-2  max-sm:max-w-35 bg-white p-5 md:p-10 max-sm:p-3 rounded-sm border-2 border-dashed border-blue-950'>
 								<h1 className='text-xl md:text-3xl max-sm:text-sm font-bold text-blue-800 italic mb-0'>
 									{text}
 								</h1>
